3eda164f3247eeed34777bc2d9332246a3fd04ea
che-ri <rnjs1135@gmail.com>
2021-06-04 00:07:08 +0900

    [feat]header 로고 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import Styled from 'styled-components';
+import logo from '../img/logo-white.png';
 
 const StyledHeader = Styled.header`
 display:flex;
 justify-content:center;
 width: 100%;
-height: 50px;
+height: 70px;
+padding: 10px 0;
 img{
   height:100%;
 }
 `;
 
 const Header = () => {
-  return <StyledHeader></StyledHeader>;
+  return (
+    <StyledHeader>
+      <img src={logo} alt="logo" />
+    </StyledHeader>
+  );
 };
 
 export default Header;
